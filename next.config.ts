831e7d04d33aa937818ffadea6bbaa3e5e3b5577
NooisY <143975472+KARLOSSONRIVO@users.noreply.github.com>
2026-04-04 22:01:38 +0800

    added deployment for Cloudflair

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.56.129.231', '10.56.41.23'],
+  output: 'export',
 };
 
 export default nextConfig;
